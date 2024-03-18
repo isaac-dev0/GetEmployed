767cc7f5771c4dd02a8657e8887127a261973bcf
Isaac Dolphin <isaac_dolphin@outlook.com>
2024-03-18 22:04:17 +0000

    First Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+const username = "David"
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello there, {username}.
+      </h1>
+    </>
+  )
+}
